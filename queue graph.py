0d356d1e1dd5f05015b2ff1117aa3db3f13b6c49
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-04-02 00:58:44 +0400

    Update queue graph.py

---
+++ queue graph.py
@@ -0,0 +1,20 @@
+import time
+import matplotlib.pyplot as plt
+from collections import deque
+
+def add_to_consultation_queue_time(n):
+    consultation_queue = deque()
+    start_time = time.time()
+    for i in range(n):
+        consultation_queue.append(i)
+    end_time = time.time()
+    return end_time - start_time
+
+patients_range = list(range(1000, 10001, 1000))
+t = [add_to_consultation_queue_time(num_patients) for num_patients in patients_range]
+
+plt.plot(patients_range, t, marker='o')
+plt.title('Time Complexity of Add to Consultation Queue')
+plt.xlabel('Number of patients (n)')
+plt.ylabel('Time Taken (s)')
+plt.show()
